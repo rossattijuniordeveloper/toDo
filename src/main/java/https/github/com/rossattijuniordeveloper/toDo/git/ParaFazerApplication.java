b54dee3636c22d4d5cf56801c1a281e3183f313e
rossattijuniordeveloper <rossattijunior@gmail.com>
2020-11-09 19:27:30 -0400

    primeirocomit

---
+++ src/main/java/https/github/com/rossattijuniordeveloper/toDo/git/ParaFazerApplication.java
@@ -0,0 +1,13 @@
+package https.github.com.rossattijuniordeveloper.toDo.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParaFazerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParaFazerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/https/github/com/rossattijuniordeveloper/toDo/git/ParaFazerApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.rossattijuniordeveloper.toDo.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParaFazerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
