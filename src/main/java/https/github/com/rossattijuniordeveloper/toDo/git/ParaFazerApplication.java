ab9a8022df26951b991c7fad4e3f9937bc6bada2
rossattijuniordeveloper <rossattijunior@gmail.com>
2020-11-09 19:50:10 -0400

    teste hello world

--- src/main/java/https/github/com/rossattijuniordeveloper/toDo/git/ParaFazerApplication.java
+++ src/main/java/https/github/com/rossattijuniordeveloper/toDo/git/ParaFazerApplication.java
@@ -2,10 +2,17 @@ package https.github.com.rossattijuniordeveloper.toDo.git;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ParaFazerApplication {
-
+	
+	@GetMapping("/")	
+	public String hello() {
+		return "E aê Xômano";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ParaFazerApplication.class, args);
 	}
